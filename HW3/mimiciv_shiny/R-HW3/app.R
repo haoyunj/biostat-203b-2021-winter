b3eec3d303977019f7d32351a219ef201e08f93a
haoyunj <haoyunj@server.ucla-biostat-203b.com>
2021-03-03 09:09:17 +0000

    HW3-done

--- HW3/mimiciv_shiny/R-HW3/app.R
+++ HW3/mimiciv_shiny/R-HW3/app.R
@@ -14,12 +14,19 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       helpText("Select a item to show the graphic and numeraic information.",
-               "Show histograms with a red line indicated for the mean 
-               and analysis table for numeric variables.",
                "Bar charts for characteristic variables.",
-               "Analysis table for date variable."),
+               "Show histograms with a red line indicated for the mean 
+               and analysis table for non-characteristic variables.",
+               "Select bins for histograms."
+               ),
       selectInput("var", label = "Choose a variable type:",
                   choices = c("Demographic", "Lab measurement", "Vitals")), 
+      
+      sliderInput("bins",
+                  "Number of bins for histograms:",
+                  min = 1,
+                  max = 50,
+                  value = 30),
       
       uiOutput("typeChanged")
       
@@ -48,8 +55,8 @@ server <- function(input, output) {
                               "deathtime", "admission_type"))
     } else if(input$var == "Lab measurement") {
       selectInput("varselected", label = "Choose a variable to display:",
-                  choices = c("bicarbonate", "calcium", " chloride creatinine",
-                              "glucose",  "magnesium", "potassium",
+                  choices = c("bicarbonate", "calcium", "chloride", 
+                              "creatinine", "glucose",  "magnesium", "potassium",
                               "sodium", "hematocrit", "wbc", "lactate"
                               
                   ))
@@ -65,7 +72,7 @@ server <- function(input, output) {
   
   output$plot <- renderPlot({
     
-    var = input$varselected
+    var <- input$varselected
     if(is.null(var)) return(NULL)
     varClass = eval(substitute(class(icu$var)))
     
@@ -73,10 +80,13 @@ server <- function(input, output) {
       ggplot(data = icu, mapping = aes_string(x = var)) + 
         geom_bar(aes_string(fill = var)) +
         theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
-    } else if (varClass == "numeric") {
-      mean = eval(substitute(mean(icu$var, na.rm = TRUE)))
+    } else if (varClass != "character") {
+      bins <- seq(eval(substitute(min(icu$var, na.rm = TRUE))), 
+                  eval(substitute(max(icu$var, na.rm = TRUE))), 
+                  length.out = input$bins + 1)
+      mean <- eval(substitute(mean(icu$var, na.rm = TRUE)))
       ggplot(data = icu, mapping = aes_string(x = var)) + 
-        geom_histogram() +
+        geom_histogram(breaks = bins) +
         geom_vline(xintercept = mean, color = "red") +
         theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
     } else {
